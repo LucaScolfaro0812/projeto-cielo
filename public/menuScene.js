3d41fcad7a1bd7b714655e53c63215238087aa54
Gabriel Pimentel <gabriel.pimentel@sou.inteli.edu.br>
2026-02-19 15:44:27 -0300

    Implementei melhorias no design do menuScene.js

--- public/menuScene.js
+++ public/menuScene.js
@@ -5,49 +5,80 @@ class MenuScene extends Phaser.Scene {
 
     preload() {
 
-        this.load.image('titulo', 'assets/titulo.png');
-        this.load.image('nuvens', 'assets/cloud-bg1.png');
-        this.load.image('cidade', 'assets/city.png')
+        this.load.image('nuvens', 'assets/cloud-bg.png');
+        this.load.image('ceu', 'assets/sky-bg.png')
+        this.load.image('titulo', 'assets/titulo-jogo.png');
+        this.load.image('loja', 'assets/stores-bg.png')
     }
     create() {
 
         const width = this.scale.width;
         const height = this.scale.height;
 
+        // Fundo céu
 
-        // Fundo simples
-        this.cameras.main.setBackgroundColor('#6FB7FF');
-
-        // Cidade
-        this.add.image(width / 2, height, 'cidade')
-            .setOrigin(0.5, 1);
-
+        this.add.tileSprite(0, 0, width, height, "ceu")
+            .setOrigin(0, 0)
+            .setScrollFactor(0); // Fica fixo na tela
 
         // Nuvens animadas
-        this.nuvens = this.add.tileSprite(0, 0, width, 250, 'nuvens').setOrigin(0, 0).setAlpha(0.6);
-        // .setScale(0.3)
-        // this.nuvens.tileScaleX = 0.3;
-        // this.nuvens.tileScaleY = 0.3;
+        this.nuvens = this.add.tileSprite(0, 0, width, 250, 'nuvens').setOrigin(0, 0).setAlpha(0.8).setScale(1.8);
 
         // Título
-        this.add.image(width / 2, height / 3, 'titulo').setOrigin(0.5);
+        const titulo = this.add.image(width / 2, height * 0.25, 'titulo').setOrigin(0.5);
 
-        // Botão 
+        titulo.setScale(width / titulo.width * 0.8);
+
+        // Loja
+        const loja = this.add.image(width / 2, height, 'loja')
+            .setOrigin(0.5, 1);
+
+        const escalaX = width / loja.width;
+        const escalaY = escalaX * 1.0;
+
+        loja.setScale(escalaX, escalaY);
+
+        // Botão
         const playButton = this.add.text(width / 2, height / 2, 'JOGAR', {
-            fontSize: '36px',
+            fontFamily: 'Poppins',
+            fontSize: '48px',
+            color: '#ffffff',
             backgroundColor: '#001caa',
-            padding: { x: 30, y: 10 }
+            padding: { x: 10, y: 5 },
+            align: 'center'
         }).setOrigin(0.5);
 
-        playButton.setInteractive();
+        // Tornar interativo
+        playButton.setInteractive({ useHandCursor: true });
 
+        // Hover: muda cor e aumenta levemente
+        playButton.on('pointerover', () => {
+            playButton.setStyle({ backgroundColor: '#6FB7FF', color: '#1B2A4A' });
+            playButton.setScale(1.1); // aumenta um pouco
+        });
+
+        playButton.on('pointerout', () => {
+            playButton.setStyle({ backgroundColor: '#1C6ED5', color: '#ffffff' });
+            playButton.setScale(1); // volta ao tamanho normal
+        });
+
+        // Efeito “bouncing” 
+        this.tweens.add({
+            targets: playButton,
+            y: playButton.y - 10,
+            duration: 800,
+            yoyo: true,
+            repeat: -1,
+            ease: 'Sine.easeInOut'
+        });
+
+        // Clique: inicia a cena do jogo
         playButton.on('pointerdown', () => {
             this.scene.start('GameScene');
         });
-
     }
 
     update() {
-        this.nuvens.tilePositionX += 0.2; // faz a textura da imagem deslizar horizontalmente, criando efeito de movimento contínuo.
+        this.nuvens.tilePositionX += 0.5; // faz a textura da imagem deslizar horizontalmente, criando efeito de movimento contínuo.
     }
 }

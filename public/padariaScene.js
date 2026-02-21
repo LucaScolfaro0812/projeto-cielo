9bbdc0c8163a773d30839e14fea332812cabe75e
Gabriel Gomes Pimentel <gabriel.pimentel@sou.inteli.edu.br>
2026-02-21 20:01:30 -0300

    feat(padaria): adicionar cena com cenario, npc e movimentacao

--- public/padariaScene.js
+++ public/padariaScene.js
@@ -6,16 +6,38 @@ class PadariaScene extends Phaser.Scene {
 
     preload() {
         // Carregamento futuro de assets da padaria
-        this.preload.image('padaria', './assets/padaria-bg.png');
+        this.load.image('padaria', 'assets/padaria-bg-2.png');
+        this.load.image('npc-padeiro', 'assets/npc.png');
+        this.load.image('player', 'assets/marcielo.png');
     }
 
     create() {
         // Estrutura inicial da cena
+        this.add.image(480, 270, 'padaria').setScale(2.1); // Cenário da padaria
+        this.add.image(550, 180, 'npc-padeiro').setScale(0.4); // NPC padeiro
+        this.player = this.add.image(100, 100, 'player').setScale(0.5); // Jogador 
 
-        this.add.image(480, 270)
+        // Configura as teclas do teclado
+        this.teclaW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.teclaA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.teclaS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.teclaD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update() {
         // Atualizações futuras
+        let velocidade = 2.5;
+
+        if (this.teclaA.isDown) {
+            this.player.x -= velocidade;
+        } else if (this.teclaD.isDown) {
+            this.player.x += velocidade;
+        }
+
+        if (this.teclaW.isDown) {
+            this.player.y -= velocidade;
+        } else if (this.teclaS.isDown) {
+            this.player.y += velocidade;
+        }
     }
 }

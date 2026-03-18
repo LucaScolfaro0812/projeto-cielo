a2681d0b46e44808b13b338380d68e86fbf31199
cassio costa <cassio.costa@sou.inteli.edu.br>
2026-03-18 14:57:59 -0300

    feat(pause): adiciono menu de pause com ESC, opções de continuar, novo jogo e menu

--- public/src/cenas/cena-cidade.js
+++ public/src/cenas/cena-cidade.js
@@ -297,6 +297,12 @@ export class GameScene extends Phaser.Scene {
         // Define nível de zoom da câmera
         this.cameras.main.setZoom(0.60);
 
+        // Abre o menu de pause ao pressionar ESC
+        this.input.keyboard.on('keydown-ESC', () => {
+            this.scene.pause();
+            this.scene.launch('pauseScene');
+        });
+
     }
 
     /**
